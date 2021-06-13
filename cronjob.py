6bcd8524e4ebb4ddaec1c788a3944df0cbe9c121
RohanK22 <rohank2205@gmail.com>
2021-06-13 19:32:42 +0530

    Adding max_instances argument

--- cronjob.py
+++ cronjob.py
@@ -1,7 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from scheduler import cronjob
 
-scheduler = BlockingScheduler()
+job_defaults = {
+    'coalesce': False,
+    'max_instances': 3
+}
+
+scheduler = BlockingScheduler(job_defaults=job_defaults)
 scheduler.add_job(cronjob, "interval", seconds=600)
 
 scheduler.start()
